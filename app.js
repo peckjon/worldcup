75fc4cfa3a970ce4d5f72cbdb3817096e52c6325
Alex Raileanu <alexraileanu@me.com>
2016-03-11 20:54:44 +0100

    I can't git

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require('express'),
+    app = module.exports.app = exports.app = express(),
+    nunjucks = require('nunjucks');
+
+
+nunjucks.configure('templates', {
+    autoescape: true,
+    express: app
+});
+
+app.set('view engine', 'nunjucks');
+app.set('views', './templates');
+
+app.get('/', function (req, res) {
+    res.render('index.html');
+});
+
+app.listen(3000, function(){
+    console.log('Server running');
+});
