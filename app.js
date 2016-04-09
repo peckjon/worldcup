ae06131830675a0d9ca8bbab178e6d1de751d0ce
Alex Raileanu <alexraileanu@me.com>
2016-04-09 12:44:41 +0200

    Forgot a commit

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 var express     = require('express'),
     app         = express(),
     nunjucks    = require('nunjucks'),
-    worldcup    = require('./src/worldcup.js'),
-    moment      = require('moment');
+    worldcup    = require('./src/worldcup'),
+    moment      = require('moment'),
+    routes      = require('./src/routes')(app);
 
 var env = nunjucks.configure('templates', {
     autoescape: true,
@@ -16,29 +17,4 @@ env.addFilter('date', function(str, format){
     return moment(str).format(format);
 });
 
-app.get('/', function (req, res) {
-    var todayMatches = worldcup.getTodayMatches('today');
-    todayMatches.then(function (data) {
-      res.render('index.html', {
-          todayMatches: data
-      });
-    });
-});
-
-app.get('/previous', function (req, res){
-    var prevMatches = worldcup.getPrevMatches('prev');
-    prevMatches.then(function (data) {
-      res.render('prev.html', {
-          prevMatches: data
-      });
-    });
-});
-
-app.get('/tomorrow', function(req, res){
-    var tomorrowMatches = worldcup.getTomorrowMatches('tomorrow');
-    tomorrowMatches.then(function (data){
-        res.render('tomorrow.html', {
-            tomorrowMatches: data
-        })
-    });
-});
+app.listen(3000);
