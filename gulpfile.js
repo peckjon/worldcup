90cc70af5dcf2de1ac54c2b549ae74dc372eb318
Alex Raileanu <alexraileanu@me.com>
2016-04-24 11:56:59 +0200

    New styles n shit

--- gulpfile.js
+++ gulpfile.js
@@ -2,42 +2,53 @@ var gulp    = require('gulp'),
     nodemon = require('gulp-nodemon'),
     sass    = require('gulp-ruby-sass'),
     notify  = require('gulp-notify'),
+    merge   = require('merge-stream'),
     paths   = {
         publicUi: './ui/public',
-        sourcesUi: './ui/sources/sass',
+        sourcesUi: './ui/sources',
         includes: './node_modules'
-    }
+    };
 
-gulp.task('default', ['watch'], function(){
-    gulp.start('nodemon', 'css', 'icons');
+gulp.task('default', ['watch'], function () {
+    gulp.start('nodemon', 'css', 'icons', 'js');
 });
 
-gulp.task('css', function(){
-    return sass(paths.sourcesUi + '/site.scss',{
-            style: 'compressed',
-            loadPath: [
-                paths.sourcesUi,
-                paths.includes + '/bootstrap-sass/assets/stylesheets',
-                paths.includes + '/font-awesome/scss'
-            ]
-        }).on("error", notify.onError(function (error) {
+gulp.task('css', function () {
+    return sass(paths.sourcesUi + '/sass/site.scss', {
+        style: 'compressed',
+        loadPath: [
+            paths.sourcesUi + '/sass',
+            paths.includes + '/bootstrap-sass/assets/stylesheets',
+            paths.includes + '/font-awesome/scss'
+        ]
+    }).on("error", notify.onError(function (error) {
             return "Error: " + error.message;
         }))
         .pipe(gulp.dest(paths.publicUi + '/css'));
 });
 
-gulp.task('nodemon', function(){
+gulp.task('js', function () {
+    var jquery = gulp.src(paths.includes + '/jquery/dist/jquery.min.js')
+        .pipe(gulp.dest(paths.publicUi + '/js/vendor'));
+
+    var bootstrap = gulp.src(paths.includes + '/bootstrap/dist/js/bootstrap.min.js')
+        .pipe(gulp.dest(paths.publicUi + '/js/vendor'));
+
+    return merge(jquery, bootstrap);
+});
+
+gulp.task('nodemon', function () {
     nodemon({
         script: 'app.js',
         ext: 'js nj'
     });
 });
 
-gulp.task('icons', function(){
+gulp.task('icons', function () {
     return gulp.src(paths.includes + '/font-awesome/fonts/**.*')
-            .pipe(gulp.dest(paths.publicUi + '/fonts'));
+        .pipe(gulp.dest(paths.publicUi + '/fonts'));
 });
 
-gulp.task('watch', function(){
-    gulp.watch(paths.sourcesUi + '/*.scss', ['css']);
+gulp.task('watch', function () {
+    gulp.watch(paths.sourcesUi + '/sass/*.scss', ['css']);
 });
