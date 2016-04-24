9c54ced9269251b1d743df1b1bd4292356b65ee5
Alex Raileanu <alexraileanu@me.com>
2016-04-24 13:56:30 +0200

    Revert to 1748a56b0da2ea53e60095a0eab370502acda488

--- src/routes/matches.js
+++ src/routes/matches.js
@@ -1,41 +1,19 @@
-var worldcup = require('../worldcup'),
-    util     = require('../util'),
-    limit    = 9;
+var worldcup = require('../worldcup');
 
 exports.past = function (req, res) {
     var pastMatches = worldcup.getMatches('past');
     pastMatches.then(function (data) {
-        var page = req.query.page,
-            url  = req.url;
-
-        if (page === undefined) {
-            page = 1;
-        }
-
         res.render('matches/past.nj', {
-            pastMatches : util.paginate(data, limit, page),
-            firstPage   : req.path,
-            page        : page,
-            pages       : Math.floor(data.length / limit),
-            previousPage: url.replace(/\d/, parseInt(page) - 1),
-            nextPage    : url.replace(/\d/, parseInt(page) + 1),
-            goToPage    : url.replace(/\d/, '')
+            pastMatches: data
         });
     });
-};
+}
 
 exports.future = function (req, res) {
     var futureMatches = worldcup.getMatches('future');
     futureMatches.then(function (data) {
-        var page = req.query.page;
-
-        if (page == null) {
-            page = 1;
-        }
-
         res.render('matches/future.nj', {
-            futureMatches: util.paginate(data, limit, page),
-            firstPage    : req.path
+            futureMatches: data
         })
     });
-};
+}
