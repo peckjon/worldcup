2d88f31b90168c472d3dd977deae257159897d82
Alex Raileanu <alexraileanu@me.com>
2016-05-07 13:37:46 +0200

    Removed some paths. fo reals this time

--- website/__init__.py
+++ website/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, static_folder='static/public')
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/phi/Desktop/node/worldcup/data/db.worldcup.sqlite'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////data/db.worldcup.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 
