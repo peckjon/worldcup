2f3a1e8f79e46de775b9677e681e49bf8a5b46be
Alex Raileanu <alexraileanu@me.com>
2016-05-08 13:25:33 +0200

    Minor change

--- utils/caching.py
+++ utils/caching.py
@@ -5,8 +5,8 @@ import json
 
 def cache(key, url):
     if redis.get(key) is None:
-        result = json.dumps(requests.get(url, params={'json': True}).json())
-        redis.set(key, result)
+        result = requests.get(url, params={'json': True}).json()
+        redis.set(key, json.dumps(result))
 
         return result
     else:
