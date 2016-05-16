12915376317607335ebfc07423a8fd05b48af0ce
Alex Raileanu <alexraileanu@me.com>
2016-05-16 19:08:32 +0200

    Minor refactoring

--- website/routes/matches.py
+++ website/routes/matches.py
@@ -1,7 +1,7 @@
 from flask import render_template
 from website import app
 from website.utils import request as req
-from website.utils.paginate import paginate
+from website.utils.paginate import paginate_data
 from flask import request
 from flask.ext.paginate import Pagination
 
@@ -14,22 +14,9 @@ def previous(page=1):
     page = int(page)
     by_date = request.args.get('by_date')
 
-    order_by = 'asc' if by_date is None else by_date
+    data = paginate_data('past', by_date, page)
 
-    data = req.make_request({
-        'type': 'past',
-        'by_date': order_by
-    })
-
-    previous_matches = paginate(data, page, PER_PAGE)
-
-    pagination = Pagination(page=page,
-                            total=len(data),
-                            per_page=PER_PAGE,
-                            record_name='matches',
-                            css_framework='bootstrap3')
-
-    return render_template('matches/past.html', pastMatches=previous_matches, pagination=pagination)
+    return render_template('matches/past.html', pastMatches=data['matches'], pagination=data['pagination'])
 
 
 @app.route('/tomorrow')
@@ -38,19 +25,6 @@ def tomorrow(page=1):
     page = int(page)
     by_date = request.args.get('by_date')
 
-    order_by = 'asc' if by_date is None else by_date
+    data = paginate_data('future', by_date, page)
 
-    data = req.make_request({
-        'type': 'future',
-        'by_date': order_by
-    })
-
-    future_matches = paginate(data, page, PER_PAGE)
-
-    pagination = Pagination(page=page,
-                            total=len(data),
-                            per_page=PER_PAGE,
-                            record_name='matches',
-                            css_framework='bootstrap3')
-
-    return render_template('matches/future.html', futureMatches=future_matches, pagination=pagination)
+    return render_template('matches/future.html', futureMatches=data['matches'], pagination=data['pagination'])
--- website/utils/paginate.py
+++ website/utils/paginate.py
@@ -1,3 +1,9 @@
+import request as req
+from flask_paginate import Pagination
+
+PER_PAGE = 9
+
+
 def paginate(arr, page, offset):
     if page < 0:
         page = 1
@@ -6,3 +12,25 @@ def paginate(arr, page, offset):
     if len(arr) < end:
         end = len(arr) - 1
     return arr[start:end]
+
+
+def paginate_data(req_type, order_by, page):
+    order_by = 'asc' if order_by is None else order_by
+
+    data = req.make_request({
+        'type': req_type,
+        'by_date': order_by
+    })
+
+    matches = paginate(data, page, PER_PAGE)
+
+    pagination = Pagination(page=page,
+                            total=len(data),
+                            per_page=PER_PAGE,
+                            record_name='matches',
+                            css_framework='bootstrap3')
+
+    return {
+        'matches': matches,
+        'pagination': pagination
+    }
