6c920672f212c627e2cff68085d656e8deb34f81
Alex Raileanu <alexraileanu@me.com>
2016-05-25 09:02:18 +0200

    Reverted back the last commit

--- build/build.py
+++ build/build.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 import subprocess
 import logging
+import os
 
 try:
-    subprocess.call('cd ..', shell=True)
-    subprocess.call(['git', 'pull', 'origin', 'master'])
+    #subprocess.call('/home/Alex/.deploy/deploy.sh', shell=True)
+    subprocess.call(['git', 'pull', 'origin', 'master'], shell=True)
+    subprocess.call(['/usr/bin/touch /tmp/deploy'], shell=True)
 except Exception as e:
     logging.error(e)
--- website/routes/webhook.py
+++ website/routes/webhook.py
@@ -1,7 +1,9 @@
 import logging
 import subprocess
 import os
+import sys
 from flask import request
+from flask import jsonify
 from website import app
 from website.utils.webhook import verify_hmac_hash
 
@@ -16,12 +18,15 @@ def github_payload():
             payload = request.get_json()
             if payload['commits'][0]['distinct']:
                 try:
-                    cmd_output = subprocess.call(base_dir + '/build/build.py')
+                    cmd_output = subprocess.call(['python3', base_dir + '/build/build.py'])
+                    #cmd_output = subprocess.call(['/bin/sh /home/Alex/.deploy/deploy.sh'])
                     # return mail_report(str(cmd_output))
                     logging.info('Pull OK')
+                    return jsonify({'msg': str(cmd_output)})
                 except Exception as error:
                     # return mail_report(str(error.output))
                     logging.error(str(error))
+                    return jsonify({'msg': str(error)})
             else:
                 return -1
 
